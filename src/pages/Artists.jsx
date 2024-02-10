0cb87ee6841024fece7182909d568982f7e855d5
CarinaPorumb <carina_porumb@yahoo.com>
2024-02-10 20:59:13 +0200

    Improved Artworks component: fixed image URL, JSON fetch, and added new data

--- src/pages/Artists.jsx
+++ src/pages/Artists.jsx
@@ -1,36 +1,42 @@
 import {useEffect, useState} from "react";
 import {Layout} from "../components/Layout";
+import {Link} from "react-router-dom";
 
 export default function Artists() {
-    const [arts, setArts] = useState([]);
+    const [artists, setArtist] = useState([]);
 
     useEffect(() => {
         fetch('https://api.artic.edu/api/v1/artists?page=2&limit=20')
             .then((response) => response.json())
             .then((data) => {
                 if (data && data.data) {
-                    setArts(data.data);
+                    setArtist(data.data);
                 }
-                // console.log(data)
+                console.log(data)
             });
     }, []);
 
     return (
         <div>
             <Layout>
-                {arts.map((art, index) => {
+                {artists.map((artist, index) => {
                     return (
-
                         <div key={index}>
-                            <p> {art.title || 'No title available'}</p>
-                            <strong>Birth Date:</strong> {art.birth_date || 'N/A'}<br/>
-                            <strong>Death Date:</strong> {art.death_date || 'N/A'}
-                            <hr/>
+                            <Link to={`/artwork/${artist.title}`}>
+                                {artist.title}
+                            </Link>
+                            {/*<h2>{artist.title || 'No name available'}</h2>*/}
+
+                            <p>Alternate Names: {artist.alt_titles ? artist.alt_titles.join(', ') : 'None'}</p>
+                            <p><strong>Birth Date:</strong> {artist.birth_date || 'Unknown'}</p>
+                            <p><strong>Death Date:</strong> {artist.death_date || 'Unknown'}</p>
+                            <p>Description:  <span dangerouslySetInnerHTML={{__html:artist.description || 'No description available'}}/></p>
+
                         </div>
-                    );
+                    )
                 })}
             </Layout>
         </div>
+    )
 
-    );
 }
--- src/pages/Artworks.jsx
+++ src/pages/Artworks.jsx
@@ -5,10 +5,8 @@ export default function Artworks() {
 
     const [artworks, setArtworks] = useState([]);
 
-    //https://collectionapi.metmuseum.org/public/collection/v1/objects/[objectID]
-
     useEffect(() => {
-        fetch('https://api.artic.edu/api/v1/artworks?limit=10')
+        fetch('https://api.artic.edu/api/v1/artworks?limit=20')
             .then((response) => response.json())
             .then((data) => {
                 if (data && data.data) {
@@ -22,23 +20,25 @@ export default function Artworks() {
     return (
         <div>
             <Layout>
-            {artworks.map((art, index) => {
-                return (
-                    <div key={index}>
-                        {art.image && <img src={art.image} alt={art.title} style={{maxWidth: '100%'}}/>}
-                        <h3>{art.title || 'No title available'}</h3>
-                        <p>Artist title: {art.artist_title}</p>
-                        <p>Type: {art.artwork_type_title}</p>
-                        {/*<p>Description: {art.description}</p>*/}
-                        <p>Place of origin: {art.place_of_origin}</p>
+                {artworks.map((art, index) => {
+                    return (
+                        <div key={index}>
+                            {<img src={`https://www.artic.edu/iiif/2/${art.image_id}/full/843,/0/default.jpg`}
+                                  alt={art.title} style={{maxWidth: '100%'}}/>}
 
-                        <p><strong>Period:</strong> {art.period || 'N/A'}</p>
-                        <p><strong>Style:</strong> {art.style || 'N/A'}</p>
-                        <img src={art.thumbnail.lqip}/>
-                        <hr/>
-                    </div>
-                );
-            })}
+                            <h2>{art.title || 'No title available'}</h2>
+                            <p>Artist: {art.artist_display || 'Unknown'}</p>
+                            <p>Type: {art.artwork_type_title || 'N/A'}</p>
+                            <p>Period: {art.date_display || 'N/A'}</p>
+                            <p>Style/Category: {art.category_titles ? art.category_titles.join(', ') : 'N/A'}</p>
+                            <p>Place of Origin: {art.place_of_origin || 'N/A'}</p>
+                            <p>Description: <span dangerouslySetInnerHTML={{__html: art.description || 'N/A'}}/></p>
+                            <p>Viewing Location: {art.on_loan_display ? <span
+                                dangerouslySetInnerHTML={{__html: art.on_loan_display}}/> : 'Check with museum'}</p>
+
+                        </div>
+                    );
+                })}
             </Layout>
         </div>
     );
